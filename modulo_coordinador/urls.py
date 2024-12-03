b4b6c99302f0d8cf3b31317864ebb1ba96feec5f
emmanuelcodedev <emmanuel.alvarez.dev@gmail.com>
2024-12-03 15:30:05 -0600

    implementatio of aspirante_registrado()

--- modulo_coordinador/urls.py
+++ modulo_coordinador/urls.py
@@ -3,7 +3,6 @@ from . import views
 app_name = 'coordinador_home'
 urlpatterns = [
     path('home_coordinador/', views.empleado_view, name='home_coordinador'),  # Define la vista de home_empleado
+    path('aspirante_dashboard/', views.aspirante_view, name='dashboard_aspirante'),
+    path('aspirante_detalles/<int:aspirante_id>/', views.detalles_aspirante, name='detalles_aspirante')
 ]
-
-
-
--- modulo_coordinador/views.py
+++ modulo_coordinador/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from login_app.decorators import role_required # Importa desde donde esté el archivo `decorators.py`
 from django.contrib.auth.decorators import login_required
-
+from form_app.models import Aspirante
+from django.shortcuts import render, get_object_or_404
 
 
 @login_required
 @role_required('CT')
 def empleado_view(request):
     return render(request, 'home_coordinador/home_coordinador.html')
+
+@role_required('CT')
+def aspirante_view(request):
+    aspirantes = Aspirante.objects.prefetch_related('residencias').all()
+    return render(request, 'home_coordinador/dashboard_aspirante.html', {'aspirantes': aspirantes})
+
+
+@role_required('CT')
+def detalles_aspirante(request, aspirante_id):
+    aspirante = get_object_or_404(Aspirante, id=aspirante_id)
+    return render(request, 'home_coordinador/detalles_aspirante.html', {'aspirante': aspirante})
